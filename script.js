b942fb43ef3bf58b6b568ee2c34e23caac62d4ca
Yan <119971453+Exm03@users.noreply.github.com>
2024-12-01 22:19:08 +0100

    2th day
    add 2nd day, anim, hover

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ calendar.onclick = function(e){
     let dateAtCalendar = '2024-12-' + numb
     console.log(dateAtCalendar, localDate)
     if (localDate == dateAtCalendar || localDate > dateAtCalendar) {
-        render()
+        render(numb)
 
     } if (dateAtCalendar > localDate) {
         alert(`Еще рано, приходи когда наступит ${numb + '.12.2024 или позже'}`)
@@ -20,12 +20,13 @@ function checkLocalTime() {
     return localDate
 }
 
-function render() {
+function render(numb) {
     let body = document.querySelector('.body')
     body.classList.remove('bodyBack')
-    body.innerHTML = `    <header>
+    if (numb == 1) {
+        body.innerHTML = `    <header>
         <div class="buttonBoxFirst">
-            <a href="./inner.html"><p  class="headerLink">STRONA GŁÓWNA</p></a>
+            <a href="./index.html"><p  class="headerLink">STRONA GŁÓWNA</p></a>
             <a href="./kontakt"><p class="headerLink hide">KONTAKT</p></a>
             
         </div>
@@ -50,4 +51,33 @@ function render() {
         
     </section>
         `
+    } if (numb == 2) {
+        body.innerHTML= `<header>
+        <div class="buttonBoxFirst">
+            <a href="./index.html"><p  class="headerLink">STRONA GŁÓWNA</p></a>
+            <a href="./kontakt"><p class="headerLink">KONTAKT</p></a>
+            
+        </div>
+    </header>
+    <section class=" mainFirst">
+        
+        <div><h1 class="titleFirst textCentr">KTÓRY   DZIŚ   JEST?</h1>
+            <p class="textCentr">2 grudnia —  drugi grudnia</p>
+            <br>
+            <br>
+            <h2 class="nameMounth nameMounthFirst">ZIMOWE PRZESĄDY I WIERZENIA</h2>
+            <br>
+            <p>Zima w polskiej tradycji ludowej jest czasem pełnym magii i wierzeń. Wiele dawnych przesądów związanych z zimą dotyczyło przewidywania pogody, obfitości plonów czy pomyślności w nadchodzącym roku. Na przykład obserwowanie pogody w dzień Wigilii miało wskazywać, jaka będzie pogoda w nowym roku. Uważano także, że gdy podczas Bożego Narodzenia pada śnieg, rok będzie obfity w plony. Innym popularnym wierzeniem było to, że wiatr w Nowy Rok zwiastuje kierunek, z którego przyjdą zmiany w życiu.
+                <br>
+                <br>
+                Zimowe przesądy miały również wymiar ochronny – wierzono, że aby uniknąć nieszczęścia, należy np. unikać wchodzenia do domu z pustymi rękami w Nowy Rok lub upewnić się, że pierwszy gość w tym dniu to mężczyzna.
+                <br>
+                <br>
+            Sprawdzić rozumienie tekstu można <a class="link" href="https://docs.google.com/forms/d/e/1FAIpQLSfkAAnjLyYg2IhYoumnC4M035j6KVqkAmc2NEdOZVSTUyoWrA/viewform">tutaj</a>,<br> 
+            a nauczyć się nowych słów <a class="link" href="https://quizlet.com/ch/977935497/112-historia-kalendarza-adwentowego-flash-cards/">tutaj</a>.</p></div>
+        <div><img class="imageFirst" src="./image/2Day.png" alt=""></div>
+    </section>`
+    }
+    
+    
 }

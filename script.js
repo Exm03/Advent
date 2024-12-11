9ed2ae41f9d9c0a4c98c110e89331a0fd3086835
hoperly <liza.simonenko00@gmail.com>
2024-12-12 01:54:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -139,7 +139,40 @@ function render(numb) {
             a nauczyć się nowych słów <a class="link" href="https://quizlet.com/ch/977935497/112-historia-kalendarza-adwentowego-flash-cards/">tutaj</a>.</p></div>
         
     </section>
-        `
+    } if (numb == 5) {
+        body.innerHTML= `<header>
+        <div class="buttonBoxFirst">
+            <a href="./index.html"><p  class="headerLink">STRONA GŁÓWNA</p></a>
+            <a href="./kontakt"><p class="headerLink hide">KONTAKT</p></a>
+            
+        </div>
+    </header>
+    <section class=" mainFirst">
+        
+        <div><h1 class="titleFirst textCentr">KTÓRY   DZIŚ   JEST?</h1>
+            <p class="textCentr">3 grudnia — trzeci grudnia</p>
+            <br>
+            <br>
+            <h2 class="nameMounth nameMounthFirst">ZIMA W POLSCE: MROŹNE PIĘKNO NATURY</h2>
+            <br>
+            <p>Pogoda w Polsce różni się w zależności od regionu.
+                <br>
+                <br>
+                Nad morzem zimy są łagodniejsze – temperatury rzadko spadają poniżej zera, częste są deszcze i silne wiatry. W centralnej i wschodniej Polsce zima jest mroźniejsza, z temperaturami do -15°C i częstymi opadami śniegu, które utrudniają komunikację.
+                <br>
+                <br>
+                W górach panują idealne warunki do sportów zimowych – gruba warstwa śniegu i niskie temperatury utrzymują się przez kilka miesięcy.
+                <br>
+                <br>
+                Częste zjawiska to śnieg, mgła, gołoledź i mroźne wiatry, które potęgują uczucie zimna. Mimo trudnych warunków zima w Polsce ma swój urok dzięki ośnieżonym krajobrazom i świątecznej atmosferze.
+                <br>
+                <br>
+            Sprawdzić rozumienie tekstu można <a class="link" href="https://docs.google.com/forms/d/1bztVkEWeQyMmAW5CrSj02vCp7DvGEYhLBnhSPMq_lZc/edit">tutaj</a>,<br> 
+            a nauczyć się nowych słów <a class="link" href="https://quizlet.com/978712898/312-flash-cards/?i=235rwg&x=1qqt">tutaj</a>.</p></div>
+        <div class="dayIamge"><img class="imageFirst" src="./image/3Day.png" alt=""></div>
+    </section>
+
+         `
     }
     
     
